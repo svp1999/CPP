57cd278744321954388a9f017fdc970163429a9c
svp1999 <78013135+svp1999@users.noreply.github.com>
2021-08-13 22:33:45 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main() {
+    
+   
+
+
+
+    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+return 0;
+}
---
+++ product.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main() {
+ double num1, num2, product;
+  cout << "Enter two numbers: ";
+
+  // stores two floating point numbers in num1 and num2 respectively
+  cin >> num1 >> num2;
+ 
+  // performs multiplication and stores the result in product variable
+  product = num1 * num2;  
+
+  cout << "Product = " << product;    
+    
+    return 0;
+}   
+   
+
+
+
+   
+
